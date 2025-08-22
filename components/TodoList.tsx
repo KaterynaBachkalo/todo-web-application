d8accb3d7fa42a8b8c4c4188ec851287ae6d4892
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-22 17:37:06 +0300

    fix

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,29 +1,39 @@
 import { Task } from "@/app/page";
 import React from "react";
 import TodoItem from "./TodoItem";
+import LoaderComponent from "@/components/LoaderComponent";
 
 interface TodoListProps {
   tasks: Task[];
   toggleStatusTask: (id: number) => void;
   deleteTask: (id: number) => void;
+  loading: boolean;
 }
 
-const TodoList = ({ tasks, toggleStatusTask, deleteTask }: TodoListProps) => {
+const TodoList = ({
+  tasks,
+  toggleStatusTask,
+  deleteTask,
+  loading,
+}: TodoListProps) => {
   return (
-    <ul className="space-y-2">
-      {tasks.length > 0 ? (
-        tasks.map((t) => (
-          <TodoItem
-            key={t.id}
-            task={t}
-            toggleStatusTask={toggleStatusTask}
-            deleteTask={deleteTask}
-          />
-        ))
-      ) : (
-        <li className="text-gray-500">No tasks available</li>
-      )}
-    </ul>
+    <>
+      {loading && <LoaderComponent />}
+
+      <ul className="space-y-2">
+        {!loading &&
+          tasks?.length > 0 &&
+          tasks.map((t) => (
+            <TodoItem
+              key={t.id}
+              task={t}
+              toggleStatusTask={toggleStatusTask}
+              deleteTask={deleteTask}
+            />
+          ))}
+        {!loading && tasks?.length === 0 && <p>No tasks found.</p>}
+      </ul>
+    </>
   );
 };
 
