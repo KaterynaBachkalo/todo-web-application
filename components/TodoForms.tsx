11994c8f52f809b62c5f80d1f76a4b939dbb47a8
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-23 15:52:24 +0300

    fix

---
+++ components/TodoForms.tsx
@@ -0,0 +1,29 @@
+import { Task } from "@/app/page";
+import AddForm from "./AddForm";
+import SearchForm from "./SearchForm";
+
+interface TodoFormProps {
+  tasks: Task[];
+  setTasks: (tasks: Task[]) => void;
+  search: string;
+  setSearch: (search: string) => void;
+  setLoading: (loading: boolean) => void;
+}
+
+const TodoForms = ({
+  tasks,
+  setTasks,
+  search,
+  setSearch,
+  setLoading,
+}: TodoFormProps) => {
+  return (
+    <>
+      <AddForm tasks={tasks} setTasks={setTasks} setLoading={setLoading} />
+
+      <SearchForm search={search} setSearch={setSearch} />
+    </>
+  );
+};
+
+export default TodoForms;
