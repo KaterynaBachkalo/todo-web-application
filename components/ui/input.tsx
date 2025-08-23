74fe4c53721bbd55946508054ac8715b48becfc8
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-23 15:53:43 +0300

    add

---
+++ components/ui/input.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        "file:text-foreground placeholder:text-amber-400 placeholder:italic selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 min-w-0 rounded-md border bg-transparent px-3 py-3 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-lg h-[50px] text-lime-600",
+        "focus-visible: focus-visible:ring-lime-300 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Input };
