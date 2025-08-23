bf69c69dae2e3d9e17113bcb4a5b9e338875fa86
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-23 15:53:24 +0300

    add components from shadcn

--- components/TodoStatusFilterButtons.tsx
+++ components/TodoStatusFilterButtons.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./ui/button";
+
 interface statusProps {
   status: "all" | "done" | "undone";
   onStatus: (status: "all" | "done" | "undone") => void;
@@ -8,15 +10,18 @@ const TodoStatusFilterButtons = ({ status, onStatus }: statusProps) => {
   return (
     <div className="flex gap-2">
       {filters.map((f) => (
-        <button
+        <Button
           key={f}
+          variant={status === f ? "secondary" : "outline"}
           onClick={() => onStatus(f)}
-          className={`px-3 py-1 rounded cursor-pointer ${
-            status === f ? "bg-blue-500 text-white" : "bg-gray-200"
+          className={`px-3 py-1 rounded cursor-pointer h-[50px] text-base group ${
+            status === f ? "bg-blue-500" : "bg-gray-200"
           }`}
         >
-          {f}
-        </button>
+          <span className="text-center transition-transform group-hover:scale-130">
+            {f}
+          </span>
+        </Button>
       ))}
     </div>
   );
