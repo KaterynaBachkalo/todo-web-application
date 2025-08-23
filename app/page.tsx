a4c6888c19c896fea41e6a6ba4124687aa6b21c9
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-23 15:51:44 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@
 import { useCallback, useEffect, useState } from "react";
 
 import TodoStatusFilterButtons from "@/components/TodoStatusFilterButtons";
-import TodoForm from "@/components/TodoForm";
+import TodoForms from "@/components/TodoForms";
 import TodoList from "@/components/TodoList";
 import TodoSorterButtons from "@/components/TodoSorterButtons";
 import { ToastContainer } from "react-toastify";
-import LoaderComponent from "../components/LoaderComponent";
 
 export type Task = {
   id: number;
@@ -83,32 +82,36 @@ const Home = () => {
   };
 
   return (
-    <div className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans min-h-screen flex flex-col items-center p-2 gap-6 sm:p-4 sm:gap-8 md:p-8 md:gap-12 2xl:p-20 2xl:gap-16">
       <ToastContainer
         position="top-right"
         autoClose={3000}
         hideProgressBar={true}
       />
 
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="max-w-2xl mx-auto p-6 space-y-4">
-          <h1 className="text-2xl font-bold">TODO web application</h1>
+      <main className="flex flex-col w-full items-center sm:items-start gap-6 md:gap-8">
+        <div className="w-full max-w-full mx-auto p-2 space-y-4 sm:max-w-lg sm:p-4 md:max-w-2xl md:p-6 2xl:max-w-4xl 2xl:p-8">
+          <h1 className="text-4xl font-bold text-center mb-10 text-lime-500 text-shadow-md text-shadow-amber-300 sm:text-3xl md:text-4xl lg:text-5xl">
+            TODO web application
+          </h1>
 
-          <TodoForm
+          <TodoForms
             tasks={tasks}
             setTasks={setTasks}
             search={search}
             setSearch={setSearch}
             setLoading={setLoading}
           />
-          <TodoStatusFilterButtons
-            status={status}
-            onStatus={(newStatus) => setStatus(newStatus)}
-          />
-          <TodoSorterButtons
-            sortStatus={sortStatus}
-            onChange={(newStatus) => setSortStatus(newStatus)}
-          />
+          <div className="flex flex-col gap-2 items-center mb-6 sm:flex-row sm:gap-4 sm:justify-between sm:mb-10">
+            <TodoStatusFilterButtons
+              status={status}
+              onStatus={(newStatus) => setStatus(newStatus)}
+            />
+            <TodoSorterButtons
+              sortStatus={sortStatus}
+              onChange={(newStatus) => setSortStatus(newStatus)}
+            />
+          </div>
           <TodoList
             tasks={tasks}
             toggleStatusTask={toggleStatusTaskCheck}
