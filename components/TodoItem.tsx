0d51991015db744c9bd3c82c42447849cfe42717
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-23 15:52:47 +0300

    add components from shadcn

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+
 import { Task } from "@/app/page";
 import { Trash2 } from "lucide-react";
 import React from "react";
@@ -10,25 +15,31 @@ export interface TodoItemProps {
 
 const TodoItem = ({ task, toggleStatusTask, deleteTask }: TodoItemProps) => {
   return (
-    <li className="flex justify-between items-center p-2 border rounded">
-      <div>
-        <input
-          type="checkbox"
-          checked={task?.status === "done"}
-          onChange={() => toggleStatusTask(task.id)}
-          className="mr-2 cursor-pointer"
-        />
-        <span className={task?.status === "done" ? "line-through" : ""}>
+    <li>
+      <Label className="hover:bg-[#f7fee780] shadow-lg hover:shadow-lime-200 shadow-lime-100  items-center gap-3 rounded-lg border p-3 has-[[aria-checked=true]]:border-lime-600 has-[[aria-checked=true]]:bg-lime-100 md:p-3 2xl:p-4 w-full">
+        <span className="transition-transform hover:scale-150">
+          <Checkbox
+            id="toggle-2"
+            onCheckedChange={() => toggleStatusTask(task.id)}
+            checked={task?.status === "done"}
+            className="data-[state=checked]:border-lime-600 cursor-pointer data-[state=checked]:bg-lime-600 data-[state=checked]:text-white dark:data-[state=checked]:border-lime-700 dark:data-[state=checked]:bg-lime-700"
+          />
+        </span>
+        <p
+          className={`text-slate-700 mr-auto sm:text-sm md:text-xl 2xl:text-2xl break-words xs:w-20 sm:w-70 md:w-100 capitalize ${
+            task?.status === "done" ? "line-through" : ""
+          }`}
+        >
           {task.text}
-        </span>
-        <span className="ml-2 text-sm text-gray-500">
+        </p>
+        <p className="text-gray-400 xs:text-xs sm:text-sm md:text-lg 2xl:text-xl min-w-17">
           (Priority {task.priority})
-        </span>
-      </div>
-      <Trash2
-        className="stroke-red-500 cursor-pointer"
-        onClick={() => deleteTask(task.id)}
-      />
+        </p>
+        <Trash2
+          className="stroke-amber-500 cursor-pointer transition-transform hover:scale-150 min-w-6 h-6"
+          onClick={() => deleteTask(task.id)}
+        />
+      </Label>
     </li>
   );
 };
