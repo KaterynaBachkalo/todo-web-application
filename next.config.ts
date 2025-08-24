804e63a06b51749f44d262ab044d503a46dba4de
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-24 12:27:58 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   images: { unoptimized: true },
+  basePath: isProd
+    ? "https://github.com/KaterynaBachkalo/todo-web-application"
+    : "",
+  assetPrefix: isProd
+    ? "https://github.com/KaterynaBachkalo/todo-web-application/"
+    : "",
 };
 
 export default nextConfig;
