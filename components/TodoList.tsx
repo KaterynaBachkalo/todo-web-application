117435a278471b04aac37f14ca34625f88c3ef25
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-24 12:10:47 +0300

    fix

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -2,6 +2,7 @@ import { Task } from "@/app/page";
 import React from "react";
 import TodoItem from "./TodoItem";
 import LoaderComponent from "@/components/LoaderComponent";
+import Image from "next/image";
 
 interface TodoListProps {
   tasks: Task[];
@@ -31,7 +32,15 @@ const TodoList = ({
               deleteTask={deleteTask}
             />
           ))}
-        {!loading && tasks?.length === 0 && <p>No tasks found.</p>}
+        {!loading && tasks?.length === 0 && (
+          <Image
+            src="/no-tasks.png"
+            alt="No tasks"
+            width={200}
+            height={200}
+            className="mx-auto mt-10 opacity-50"
+          />
+        )}
       </ul>
     </>
   );
