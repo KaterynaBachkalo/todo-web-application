15996c152f51cb9aec622ca14a7e6bc53308d01a
KaterynaBachkalo <kozlovets88@gmail.com>
2025-09-24 11:48:09 +0300

    add drug&drop for list in todolist

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -4,8 +4,25 @@ import LoaderComponent from "@/components/LoaderComponent";
 import TodoItem from "./TodoItem";
 import { Task } from "./types";
 
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
 interface TodoListProps {
   tasks: Task[];
+  setTasks: (tasks: Task[]) => void;
   toggleStatusTask: (id: number) => void;
   deleteTask: (id: number) => void;
   loading: boolean;
@@ -13,35 +30,71 @@ interface TodoListProps {
 
 const TodoList = ({
   tasks,
+  setTasks,
   toggleStatusTask,
   deleteTask,
   loading,
 }: TodoListProps) => {
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    if (active.id !== over.id) {
+      const oldIndex = tasks.findIndex((t) => t.id === Number(active.id));
+      const newIndex = tasks.findIndex((t) => t.id === Number(over.id));
+
+      if (oldIndex !== -1 && newIndex !== -1) {
+        setTasks(arrayMove(tasks, oldIndex, newIndex));
+      }
+    }
+  };
+
   return (
     <>
       {loading && <LoaderComponent />}
 
-      <ul className="space-y-4">
-        {!loading &&
-          tasks?.length > 0 &&
-          tasks.map((t) => (
-            <TodoItem
-              key={t.id}
-              task={t}
-              toggleStatusTask={toggleStatusTask}
-              deleteTask={deleteTask}
-            />
-          ))}
-        {!loading && tasks?.length === 0 && (
-          <Image
-            src="no-tasks.png"
-            alt="No tasks"
-            width={200}
-            height={200}
-            className="mx-auto mt-10 opacity-50"
-          />
-        )}
-      </ul>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          items={tasks.map((t) => {
+            return t.id.toString();
+          })}
+          strategy={verticalListSortingStrategy}
+        >
+          <ul className="space-y-4">
+            {!loading &&
+              tasks?.length > 0 &&
+              tasks.map((t) => (
+                <TodoItem
+                  key={t.id}
+                  task={t}
+                  toggleStatusTask={toggleStatusTask}
+                  deleteTask={deleteTask}
+                />
+              ))}
+            {!loading && tasks?.length === 0 && (
+              <Image
+                src="no-tasks.png"
+                alt="No tasks"
+                width={200}
+                height={200}
+                className="mx-auto mt-10 opacity-50"
+              />
+            )}
+          </ul>
+        </SortableContext>
+      </DndContext>
     </>
   );
 };
